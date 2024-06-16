877bc9c3702988d01935b5a19f3d08a1da1f49cc
Pouyan <154380480+Mrengin1@users.noreply.github.com>
2024-06-16 15:20:59 -0400

    Create Password generator.py

---
+++ Password generator.py
@@ -0,0 +1,10 @@
+import random
+import string
+
+def generate_password(length):
+    characters = string.ascii_letters + string.digits + string.punctuation
+    password = ''.join(random.choice(characters) for i in range(length))
+    return password
+
+# Example usage: Generates a 12-character long password
+print(generate_password(12))
